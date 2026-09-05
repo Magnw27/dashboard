c175f107a184bfbb35d48f6bf6a8ff54a89379d1
lipcyy <ripzzbg@gmail.com>
2026-09-05 18:30:01 +0700

    feat: add React footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="footer shell">
+      <span>© 2026 LIPZCODE</span>
+      <a href="https://github.com/Magnw27" target="_blank" rel="noreferrer">github.com/Magnw27 ↗</a>
+    </footer>
+  );
+}
